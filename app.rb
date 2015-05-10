6deefc63799ce6ae403f69b5154b3daa39dbd9a3
Rémy Hannequin <remy.hannequin@gmail.com>
2015-05-11 00:58:00 +0200

    Start parsing CSV file

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require 'active_support'
+require 'smarter_csv'
+require 'i18n'
 
 module Accounting
   class App < Sinatra::Base
@@ -11,6 +14,14 @@ module Accounting
     end
 
     get '/' do
+      SmarterCSV.process('data.csv',  headers_in_file: true,
+                                      file_encoding: 'utf-8',
+                                      col_sep: ';',
+                                      row_sep: "\n") do |chunk|
+        chunk.each do |c|
+          date = Date.strptime(c[:date], '%d/%m/%y')
+        end
+      end
       '<h1>Hello World!</h1>'
     end
   end
