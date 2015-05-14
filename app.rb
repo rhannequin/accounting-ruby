6f770dfb64a59813875c761efa43646094e70a53
Rémy Hannequin <remy.hannequin@gmail.com>
2015-05-15 01:51:23 +0200

    Display current money amount

--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ module Accounting
   class App < Sinatra::Base
     set :environments, %w(production development test)
     set :environment, (ENV['RACK_ENV'] || ENV['SPACEAPI_APPLICATION_ENV'] || :development).to_sym
+    set :initial_money, 100
 
     configure :development do
       register Sinatra::Reloader
@@ -16,32 +17,33 @@ module Accounting
 
     get '/' do
       data = nil
+      current_money = settings.initial_money
       SmarterCSV.process('data.csv',  chunk_size: 10000,
                                       headers_in_file: true,
                                       file_encoding: 'utf-8',
                                       col_sep: ';',
                                       row_sep: "\n",
-                                      remove_empty_values: false,
-                                      remove_zero_values: false,
-                                      remove_unmapped_keys: false,
-                                      remove_empty_hashes: false,
                                       strip_whitespace: true,
                                       key_mapping: {
                                         date: :date,
-                                        objet: :object,
+                                        objet: :reason,
                                         prix: :price,
                                         moyen: :way,
                                         catégories: :categories,
                                       }) do |chunk|
         data = chunk
         data.map! do |c|
+          current_money += c[:price]
           c[:date] = Date.strptime(c[:date], '%d/%m/%y')
-          c[:categories] = c[:categories].split(',').map(&:strip) unless c[:categories].nil?
+          c[:categories] = c[:categories].nil? ? [] : c[:categories].split(',').map(&:strip)
           c
         end
       end
       data = data.group_by { |x| x[:date].beginning_of_month }.sort.reverse
-      haml :index, locals: { data: data }
+      data.each do |_, exps|
+        exps.sort_by! { |exp| exp[:date] }.reverse!
+      end
+      haml :index, locals: { data: data, current_money: current_money }
     end
   end
 end
