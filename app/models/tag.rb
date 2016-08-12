3f3f80a42ff245fe5ac833460f0c588e386a5f33
Rémy Hannequin <remy.hannequin@gmail.com>
2016-08-12 16:59:11 +0200

    Start using friendly_id

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,14 @@
 class Tag < ApplicationRecord
+  extend FriendlyId
+  friendly_id :name, use: [:slugged, :finders]
+
   has_many :taggings, dependent: :destroy
   has_many :expenses, through: :taggings, source: :taggable, source_type: 'Expense'
   has_many :debits, through: :taggings, source: :taggable, source_type: 'Debit'
 
   scope :ignored, -> { where(ignored: true) }
+
+  def should_generate_new_friendly_id?
+    name_changed? || super
+  end
 end
