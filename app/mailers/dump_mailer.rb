02cf73527760803f5851362f820d0121f47faf94
Rémy Hannequin <remy.hannequin@gmail.com>
2017-08-17 12:03:28 +0200

    Create task db:dump:email to send last backup dump by email

---
+++ app/mailers/dump_mailer.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class DumpMailer < ApplicationMailer
+  def dump_email(to, dump)
+    attachments[File.basename(dump)] = File.read(dump)
+    app_name = I18n.t(:'layouts.application_name')
+    mail(to: to,
+         subject: I18n.t(
+          :'dump_mailer.dump_email.subject',
+          app_name: app_name))
+  end
+end
